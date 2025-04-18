767669896e83e11a5874750133c2f7dc97ed8410
thanosshawn <thanosshawn@gmail.com>
2025-04-18 16:21:01 +0530

    first react

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB7AaSc-eQbv78m6jKxSE-FMYuAvpgvx8o",
+  authDomain: "streamspeak-81086.firebaseapp.com",
+  projectId: "streamspeak-81086",
+  storageBucket: "streamspeak-81086.firebasestorage.app",
+  messagingSenderId: "244558748719",
+  appId: "1:244558748719:web:12acdc22c9d12a68c5bada",
+  measurementId: "G-2VY44XHBQ3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
